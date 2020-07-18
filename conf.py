ccacf8b1d33680b5905e47fa1b65a66d5c016f9c
Bhargav Vummadi <52027911+Lusr77@users.noreply.github.com>
2020-07-18 14:32:12 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'Web Design and Development - Bharagv'
+copyright = '2020, Bhargav'
+author = 'Bhargav'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
